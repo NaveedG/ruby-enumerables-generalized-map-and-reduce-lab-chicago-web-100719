23cdb01c5dd8368c9255e4764b4968f864feb8e9
Naveed Ganjani <nganjani@protonmail.com>
2019-09-23 20:56:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point= nil)
+def reduce(source_array, starting_point= 0)
   if starting_point
     result = starting_point
     x = 0
